18c076dc0f4422f68978ffb75c3fc18b780ed2e8
hellgate202 <hellgate.azuregos@gmail.com>
2019-02-24 20:37:03 +0300

    changed type of parameter

--- src/dual_port_ram.sv
+++ src/dual_port_ram.sv
@@ -1,7 +1,6 @@
 module dual_port_ram #(
   parameter int    DATA_WIDTH        = 8,
   parameter int    ADDR_WIDTH        = 5,
-  parameter int    REGISTERED_OUTPUT = "FALSE",
   parameter string INIT_FILE         = ""
 )(
   input                       rst_i,
@@ -11,14 +10,12 @@ module dual_port_ram #(
   input                       wr_i,
   input                       rd_clk_i,
   input  [ADDR_WIDTH - 1 : 0] rd_addr_i,
-  input                       output_reg_en_i,
   output [DATA_WIDTH - 1 : 0] rd_data_o,
   input                       rd_i
 );
 
 logic [DATA_WIDTH - 1 : 0] ram [2 ** ADDR_WIDTH - 1 : 0];
 logic [DATA_WIDTH - 1 : 0] rd_data;
-logic [DATA_WIDTH - 1 : 0] rd_data_reg = '0;
 
 initial
   if( INIT_FILE != "" )
@@ -32,13 +29,6 @@ always_ff @( posedge rd_clk_i )
   if( rd_i )
     rd_data <= ram[rd_addr_i];
 
-always_ff @( posedge rd_clk_i )
-  if( rst_i )
-    rd_data_reg <= '0;
-  else
-    if( output_reg_en_i )
-      rd_data_reg <= rd_data;
-
-assign rd_data_o = ( REGISTERED_OUTPUT ) ? rd_data_reg : rd_data;
+assign rd_data_o = rd_data;
 
 endmodule
