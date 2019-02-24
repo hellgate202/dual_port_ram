e967e9b18134b6733539ff145182efd934e4b11d
hellgate202 <hellgate.azuregos@gmail.com>
2019-02-24 17:22:54 +0300

    coding style fixes

--- src/dual_port_ram.sv
+++ src/dual_port_ram.sv
@@ -1,24 +1,24 @@
 module dual_port_ram #(
-  parameter DATA_WIDTH        = 8,
-  parameter ADDR_WIDTH        = 5,
-  parameter REGISTERED_OUTPUT = "FALSE",
-  parameter INIT_FILE         = ""
+  parameter int    DATA_WIDTH        = 8,
+  parameter int    ADDR_WIDTH        = 5,
+  parameter int    REGISTERED_OUTPUT = "FALSE",
+  parameter string INIT_FILE         = ""
 )(
-  input                   rst_i,
-  input                   wr_clk_i,
-  input [ADDR_WIDTH-1:0]  wr_addr_i,
-  input [DATA_WIDTH-1:0]  wr_data_i,
-  input                   wr_i,
-  input                   rd_clk_i,
-  input  [ADDR_WIDTH-1:0] rd_addr_i,
-  input                   output_reg_en_i,
-  output [DATA_WIDTH-1:0] rd_data_o,
-  input                   rd_i
+  input                       rst_i,
+  input                       wr_clk_i,
+  input [ADDR_WIDTH - 1 : 0]  wr_addr_i,
+  input [DATA_WIDTH - 1 : 0]  wr_data_i,
+  input                       wr_i,
+  input                       rd_clk_i,
+  input  [ADDR_WIDTH - 1 : 0] rd_addr_i,
+  input                       output_reg_en_i,
+  output [DATA_WIDTH - 1 : 0] rd_data_o,
+  input                       rd_i
 );
 
-logic [DATA_WIDTH-1:0] ram [2**ADDR_WIDTH-1:0];
-logic [DATA_WIDTH-1:0] rd_data;
-logic [DATA_WIDTH-1:0] rd_data_reg = '0;
+logic [DATA_WIDTH - 1 : 0] ram [2 ** ADDR_WIDTH - 1 : 0];
+logic [DATA_WIDTH - 1 : 0] rd_data;
+logic [DATA_WIDTH - 1 : 0] rd_data_reg = '0;
 
 initial
   if( INIT_FILE != "" )
@@ -39,6 +39,6 @@ always_ff @( posedge rd_clk_i )
     if( output_reg_en_i )
       rd_data_reg <= rd_data;
 
-assign rd_data_o = ( REGISTERED_OUTPUT == "TRUE" ) ? rd_data_reg : rd_data;
+assign rd_data_o = ( REGISTERED_OUTPUT ) ? rd_data_reg : rd_data;
 
 endmodule
