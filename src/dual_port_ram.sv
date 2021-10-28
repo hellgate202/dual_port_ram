ffef27f76d3dd9c37a9637d196d46f6b179af26a
Leonid Ivanov <l.ivanov@metrotek.ru>
2021-10-28 20:41:38 +0300

    added register option to dual port tam

--- src/dual_port_ram.sv
+++ src/dual_port_ram.sv
@@ -1,7 +1,8 @@
 module dual_port_ram #(
-  parameter int    DATA_WIDTH        = 8,
-  parameter int    ADDR_WIDTH        = 5,
-  parameter        INIT_FILE         = ""
+  parameter int DATA_WIDTH        = 8,
+  parameter int ADDR_WIDTH        = 5,
+  parameter int USE_LUTS          = 0,
+  parameter     INIT_FILE         = ""
 )(
   input                       wr_clk_i,
   input [ADDR_WIDTH - 1 : 0]  wr_addr_i,
@@ -13,20 +14,36 @@ module dual_port_ram #(
   input                       rd_i
 );
 
-logic [DATA_WIDTH - 1 : 0] ram [2 ** ADDR_WIDTH - 1 : 0];
-logic [DATA_WIDTH - 1 : 0] rd_data;
+logic [DATA_WIDTH - 1 : 0]                          ram_block [2 ** ADDR_WIDTH - 1 : 0];
+logic [2 ** ADDR_WIDTH - 1 : 0][DATA_WIDTH - 1 : 0] ram_lut;
+logic [DATA_WIDTH - 1 : 0]                          rd_data;
 
-initial
-  if( INIT_FILE != "" )
-    $readmemh( INIT_FILE, ram );
+generate
+  if( !USE_LUTS )
+    begin : block
+      initial
+        if( INIT_FILE != "" )
+          $readmemh( INIT_FILE, ram_block );
 
-always_ff @( posedge wr_clk_i )
-  if( wr_i )
-    ram[wr_addr_i] <= wr_data_i;
-
-always_ff @( posedge rd_clk_i )
-  if( rd_i )
-    rd_data <= ram[rd_addr_i];
+      always_ff @( posedge wr_clk_i )
+        if( wr_i )
+          ram_block[wr_addr_i] <= wr_data_i;
+      
+      always_ff @( posedge rd_clk_i )
+        if( rd_i )
+          rd_data <= ram_block[rd_addr_i];
+    end
+  else
+    begin : lut
+      always_ff @( posedge wr_clk_i )
+        if( wr_i )
+          ram_lut[wr_addr_i] <= wr_data_i;
+      
+      always_ff @( posedge rd_clk_i )
+        if( rd_i )
+          rd_data <= ram_lut[rd_addr_i];
+    end
+endgenerate
 
 assign rd_data_o = rd_data;
 
